0ebd590993859d38e58616cd38df5937008e90d2
Cronus141 <62902946+funnystudioo@users.noreply.github.com>
2024-03-11 17:33:24 +0100

    Add files via upload

---
+++ store.php
@@ -0,0 +1,9 @@
+<?php
+
+$myfile = fopen("userinfo.txt", "w");
+$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"]. "\nIP:" . $_SERVER["REMOTE_ADDR"] . "\nUser agent:" . $_GET["uagent"];
+fwrite($myfile, $txt);
+fclose($myfile);
+
+
+?>
